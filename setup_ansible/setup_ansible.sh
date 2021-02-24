ce16daf83641f3885706d693a631ded99cf0c501
chhabrakaran7 <chhabrakaran2020@gmail.com>
2021-02-24 00:33:22 +0000

    Added ansible installation validations in the ansible_setup.sh script.

--- setup_ansible/setup_ansible.sh
+++ setup_ansible/setup_ansible.sh
@@ -7,3 +7,15 @@ dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.r
 
 #Enable ansible repo and install ansible#
 yum install --enablerepo epel-playground ansible -y
+
+#Verify ansible installation#
+ansible --version | head -1
+
+if [[ "$?" -eq 0 ]]; then
+        ans_version=`ansible --version | head -1`
+        echo "Ansible is successfully installed and the version installed is "$ans_version""
+        exit 0
+else
+        echo "Ansible is not configured correctly on the system. Please verify installation"
+        exit 1
+fi
